7411847fcfa4525830e8da1b1e519b9ea7b6daf3
ex_zhangwenlei@exiot.cmcc <15196725410@139.com>
2023-07-18 20:58:02 +0800

    feat: something new

--- src/template-nestjs/src/app.module.ts
+++ src/template-nestjs/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
+import { PrismaModule } from './shared/common/prisma/prisma.module';
+
+import envConfig from '../config/env';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: [envConfig.envPath],
+        }),
+        PrismaModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+
 })
-export class AppModule {}
+export class AppModule { }
